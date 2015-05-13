f76bdb2484a21fd3d096fc59a0bdb484e3ac60f6
ulinke@hotmail.com <ulinke@hotmail.com>
2015-05-13 17:09:36 +0800

    Version update to phpb2b5.0.5.001

--- phpb2b_version.php
+++ phpb2b_version.php
@@ -6,6 +6,6 @@
  *
  *      @version $Revision: 2959 $
  */
-define('PHPB2B_VERSION', '5.0.4');
-define('PHPB2B_RELEASE', '20141017');
+define('PHPB2B_VERSION', '5.0.5.001');
+define('PHPB2B_RELEASE', '20150513');
 ?>
