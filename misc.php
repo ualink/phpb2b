ffd5c60c6cfbc674bb3081efba47fb824bceed08
stevenzxx <zhouxinxin@cri.cn>
2019-06-12 10:28:21 +0800

    [FIX] Secure fix

--- misc.php
+++ misc.php
@@ -3,7 +3,6 @@
  *      [PHPB2B] Copyright (C) 2007-2099, Ualink Inc. All Rights Reserved.
  *      The contents of this file are subject to the License; you may not use this file except in compliance with the License. 
  *
- *      @version $Revision: 2075 $
  */
 define('CURSCRIPT', 'attachment');
 require("libraries/common.inc.php");
