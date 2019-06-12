c67dbca78eaba0be7e28fb1be131e090b6d9679b
stevenzxx <zhouxinxin@cri.cn>
2019-06-12 08:29:16 +0800

    Upgrade captcha

--- libraries/core/paths.php
+++ libraries/core/paths.php
@@ -5,19 +5,19 @@
  *
  *      @version $Revision: 2048 $
  */
-define('APP_PATH', PHPB2B_ROOT .'app'.DS);
-define('PLUGIN_PATH', PHPB2B_ROOT .'plugins'.DS);
-define('SLUGIN_PATH', PHPB2B_ROOT .'libraries'.DS.'slug'.DS);
+if(!defined('APP_PATH')) define('APP_PATH', PHPB2B_ROOT .'app'.DS);
+if(!defined('PLUGIN_PATH')) define('PLUGIN_PATH', PHPB2B_ROOT .'plugins'.DS);
+if(!defined('SLUGIN_PATH')) define('SLUGIN_PATH', PHPB2B_ROOT .'libraries'.DS.'slug'.DS);
 //Cache sets
-define('DATA_PATH', PHPB2B_ROOT."data".DS);
-define('CACHE_ROOT', PHPB2B_ROOT."data".DS."cache".DS);
-define('API_PATH', PHPB2B_ROOT."api".DS);
+if(!defined('DATA_PATH')) define('DATA_PATH', PHPB2B_ROOT."data".DS);
+if(!defined('CACHE_ROOT')) define('CACHE_ROOT', PHPB2B_ROOT."data".DS."cache".DS);
+if(!defined('API_PATH')) define('API_PATH', PHPB2B_ROOT."api".DS);
 if (!defined('PHP5')) {
 	define('PHP5', (PHP_VERSION >= 5));
 }
 if(!defined('SOURCE_PATH')) define('SOURCE_PATH',PHPB2B_ROOT.'libraries'.DS);
 if(!defined('CLASS_PATH')) define('CLASS_PATH',PHPB2B_ROOT.'libraries'.DS.'source'.DS);
 if(!defined('LIB_PATH')) define('LIB_PATH',PHPB2B_ROOT.'libraries'.DS);
-define('STATICURL', !empty($staticurl) ? $staticurl : 'static/');
-define('JSMIN_AS_LIB', true); // prevents auto-run on include
+if(!defined('STATICURL')) define('STATICURL', !empty($staticurl) ? $staticurl : 'static/');
+if(!defined('JSMIN_AS_LIB')) define('JSMIN_AS_LIB', true); // prevents auto-run on include
 ?>
