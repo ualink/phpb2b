a12d47695cf000e45cebd280a1bac582c18e719e
steven <zhouxin@wts.cn>
2019-10-15 22:48:21 +0800

    Fix Chinese language bug when installing

--- libraries/global.func.php
+++ libraries/global.func.php
@@ -1012,7 +1012,7 @@ function pb_configmake($lang, $exit = true)
 				file_put_contents($config_file, $title . " = \"" . $content . "\"\r\n", FILE_APPEND);
 			}
 		} else {
-			while ($data = fgetcsv($fp, 1024, ",")) {
+			while ($data = _fgetcsv($fp, 1024, ",")) {
 				$title = trim($data[0]);
 				$content = $data[1];
 				if(strpos($content, "\r\n")!==false){
@@ -1040,14 +1040,9 @@ function pb_configmake($lang, $exit = true)
 					file_put_contents($config_file, $title . " = \"" . $content . "\"\r\n", FILE_APPEND);
 				}
 			} else {
-				while ($data = fgetcsv($fp, 1024, ",")) {
+				while ($data = _fgetcsv($fp, 1024, ",")) {
 					$title = trim($data[0]);
 					$content = $data[1];
-                    if(strpos($content, "\r\n")!==false){
-                        $content = strstr($content, "\r\n", true);
-                    }elseif(strpos($content, "\n")!==false){
-                        $content = strstr($content, "\n", true);
-                    }
 					if ($charset == "gbk") {
 						//only for gbk chinese
 						$content = iconv('gbk', $charset, $content); //if your language is utf-8,please delete this line.
@@ -1067,6 +1062,28 @@ function pb_configmake($lang, $exit = true)
 		header_sent("Wrong with language maken, or language " . $lang . " not exists.");
 		exit;
 	}
+}
+
+function _fgetcsv(& $handle, $length = null, $d = ',', $e = '"') {
+    $d = preg_quote($d);
+    $e = preg_quote($e);
+    $_line = "";
+    $eof=false;
+    while ($eof != true) {
+        $_line .= (empty ($length) ? fgets($handle) : fgets($handle, $length));
+        $itemcnt = preg_match_all('/' . $e . '/', $_line, $dummy);
+        if ($itemcnt % 2 == 0)
+            $eof = true;
+    }
+    $_csv_line = preg_replace('/(?: |[ ])?$/', $d, trim($_line));
+    $_csv_pattern = '/(' . $e . '[^' . $e . ']*(?:' . $e . $e . '[^' . $e . ']*)*' . $e . '|[^' . $d . ']*)' . $d . '/';
+    preg_match_all($_csv_pattern, $_csv_line, $_csv_matches);
+    $_csv_data = $_csv_matches[1];
+    for ($_csv_i = 0; $_csv_i < count($_csv_data); $_csv_i++) {
+        $_csv_data[$_csv_i] = preg_replace('/^' . $e . '(.*)' . $e . '$/s', '$1', $_csv_data[$_csv_i]);
+        $_csv_data[$_csv_i] = str_replace($e . $e, $e, $_csv_data[$_csv_i]);
+    }
+    return empty ($_line) ? false : $_csv_data;
 }
 
 function array_multi2single($array)
