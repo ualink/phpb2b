e54f1248a0c8b7a847f6e420163526db4be9c36d
steven <ualink@hotmail.com>
2022-05-29 21:19:19 +0800

    Update version number

--- phpb2b_version.php
+++ phpb2b_version.php
@@ -1,11 +1,10 @@
 <?php
 /**
  *      [PHPB2B] Copyright (C) 2007-2099, Ualink Inc. All Rights Reserved.
- *      The contents of this file are subject to the License; 
- *		You may not use this file except in compliance with the License. 
+ *      The contents of this file are subject to the License;
+ *		You may not use this file except in compliance with the License.
  *
  *      @version $Revision: 2959 $
  */
-define('PHPB2B_VERSION', '6.0');
-define('PHPB2B_RELEASE', '20190611');
-?>
+define('PHPB2B_VERSION', '6.0.4');
+define('PHPB2B_RELEASE', '20220523');
